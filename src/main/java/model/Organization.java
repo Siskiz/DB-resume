3d166fcfb72e5c666b16b22585809582f41c355a
Siskiz <markevi4.Alex@yandex.ru>
2022-08-10 23:26:10 +0300

    Complete DataStreamSerializer
    Add full realization DataStreamSerializer

--- src/main/java/model/Organization.java
+++ src/main/java/model/Organization.java
@@ -40,6 +40,14 @@ public class Organization implements Serializable {
         this.positions = positions;
     }
 
+    public Link getHomePage() {
+        return homePage;
+    }
+
+    public List<Position> getPositions() {
+        return positions;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -87,7 +95,7 @@ public class Organization implements Serializable {
             this.startDate = startDate;
             this.endDate = endDate;
             this.title = title;
-            this.description = description;
+            this.description = description == null ? "" : description;
         }
 
         public LocalDate getStartDate() {
@@ -104,6 +112,14 @@ public class Organization implements Serializable {
 
         public void setEndDate(LocalDate endDate) {
             this.endDate = endDate;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescription() {
+            return description;
         }
 
         @Override
