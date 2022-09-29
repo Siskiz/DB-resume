ba27c4adee561374c7752942e147bd98d7a6a75e
Siskiz <markevi4.Alex@yandex.ru>
2022-09-29 19:04:22 +0300

    small fix servlet

--- EE/src/main/java/com/example/ee/HelloServlet.java
+++ EE/src/main/java/com/example/ee/HelloServlet.java
@@ -1,13 +1,14 @@
 package com.example.ee;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "hello", urlPatterns = {"/hello"})
+@WebServlet(name = "hello", value = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
