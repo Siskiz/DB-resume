4b18dfaab25008ccd3599be6f95bbf46a0a2e2a5
Siskiz <markevi4.Alex@yandex.ru>
2022-09-29 20:58:28 +0300

    Update HelloServlet.java

--- EE/src/main/java/com/example/ee/HelloServlet.java
+++ EE/src/main/java/com/example/ee/HelloServlet.java
@@ -18,13 +18,21 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
 
-        // Hello
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        if (name == null) {
+            // Hello
+            out.println("<html><body>");
+            out.println("<h1>" + message + "</h1>");
+            out.println("</body></html>");
+        } else {
+            out.println("<html><body>");
+            out.println("<h1>" + "Hello " + name + "!" + "</h1>");
+            out.println("</body></html>");
+        }
     }
 
     public void destroy() {
